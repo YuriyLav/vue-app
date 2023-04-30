692162569016a803cad90cd654d8fc48a65b43ec
YuriyLav <yurii.lvrs@gmail.com>
2023-04-30 19:24:06 +0300

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div class="app">
+        <post-form  @create="createPost"/>
+        <post-list v-bind:posts="posts"/>
+    </div>
 </template>
 
+<script>
+import PostForm from './components/PostForm.vue'
+import PostList from '@/components/PostList.vue'
+
+export default {
+    components: {
+        PostForm, PostList
+    },
+    data() {
+        return {
+            posts: [
+                { id: 1, title: 'Javascript1', body: 'Опсиание поста1' },
+                { id: 2, title: 'Javascript2', body: 'Опсиание поста2' },
+                { id: 3, title: 'Javascript3', body: 'Опсиание поста3' },
+                { id: 4, title: 'Javascript4', body: 'Опсиание поста4' },
+            ],
+        }
+    },
+    methods: {
+        createPost(post) {
+            this.posts.push(post)
+        }
+    }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app {
+    padding: 20px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
